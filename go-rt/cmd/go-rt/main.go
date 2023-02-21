899f7ac54a358459837b64493134659fb33170f3
wmbat <wmbat-dev@protonmail.com>
2023-02-21 11:19:25 -0500

    Added new scene

--- go-rt/cmd/go-rt/main.go
+++ go-rt/cmd/go-rt/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-	"runtime/pprof"
+	"math/rand"
+	"time"
 
 	"github.com/wmbat/ray_tracer/internal/maths"
 	"github.com/wmbat/ray_tracer/internal/render"
@@ -17,78 +15,96 @@ const aspectRatio float64 = 16.0 / 9.0
 const imageWidth int = 1280
 const imageHeight int = int((float64(imageWidth) / aspectRatio))
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-
 func main() {
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close() // error handling omitted for example
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
-
-	lookFrom := maths.Point3{X: -3, Y: 3, Z: 2}
-	lookAt := maths.Point3{X: 0, Y: 0, Z: -1}
+	lookFrom := maths.Point3{X: 13, Y: 2, Z: 3}
+	lookAt := maths.Point3{X: 0, Y: 0, Z: 0}
 	cameraInfo := world.CameraCreateInfo{
-		LookFrom:    lookFrom,
-		LookAt:      lookAt,
-		Up:          maths.Vec3{X: 0, Y: 1, Z: 0},
-		Fov:         20,
-		AspectRatio: aspectRatio,
-		Aperture:    2.0,
-		FocusDistance: lookFrom.Sub(lookAt).ToVec3().Length(),}
+		LookFrom:      lookFrom,
+		LookAt:        lookAt,
+		Up:            maths.Vec3{X: 0, Y: 1, Z: 0},
+		Fov:           20,
+		AspectRatio:   aspectRatio,
+		Aperture:      0.1,
+		FocusDistance: 10}
 
 	camera := world.NewCamera(cameraInfo)
 
-	sceneName := "Test Scene"
+	scene := GenerateRandomScene()
 
-	mainScene := world.NewScene(sceneName)
-	mainScene.SetEnvironmentColour(render.Colour{Red: 135 / 256.0, Green: 206 / 256.0, Blue: 235 / 256.0})
-	mainScene.AddEntity(entt.Sphere{
-		Position: maths.Point3{X: 0, Y: 0, Z: -1},
-		Radius:   0.5,
-		Material: mats.Lambertian{Albedo: render.Colour{Red: 0.1, Green: 0.2, Blue: 0.5}}})
-
-	// Floor
-	mainScene.AddEntity(entt.Sphere{
-		Position: maths.Point3{X: 0, Y: -100.5, Z: -1},
-		Radius:   100,
-		Material: mats.Lambertian{Albedo: render.Colour{Red: 0.8, Green: 0.8, Blue: 0.0}}})
-
-	// Glass
-	mainScene.AddEntity(entt.Sphere{
-		Position: maths.Point3{X: -1, Y: 0, Z: -1},
-		Radius:   0.5,
-		Material: mats.Dielectric{
-			Diffuse:         render.Colour{Red: 1.0, Green: 1.0, Blue: 1.0},
-			RefractionIndex: 1.5}})
-
-	mainScene.AddEntity(entt.Sphere{
-		Position: maths.Point3{X: -1, Y: 0, Z: -1},
-		Radius:   -0.4,
-		Material: mats.Dielectric{
-			Diffuse:         render.Colour{Red: 1.0, Green: 1.0, Blue: 1.0},
-			RefractionIndex: 1.5}})
-
-	// Metal
-	mainScene.AddEntity(entt.Sphere{
-		Position: maths.Point3{X: 1, Y: 0, Z: -1},
-		Radius:   0.5,
-		Material: mats.Metal{
-			Albedo:    render.Colour{Red: 0.8, Green: 0.6, Blue: 0.2},
-			Roughness: 0.0}})
-
-	image := mainScene.Render(camera, world.ImageRenderConfig{
+	image := scene.Render(camera, world.ImageRenderConfig{
 		ImageSize:   maths.Size2[int]{Width: imageWidth, Height: imageHeight},
-		SampleCount: 1000,
-		BounceDepth: 100,
+		SampleCount: 500,
+		BounceDepth: 50,
 	})
 
-	image.SaveAsPPM(sceneName)
+	image.SaveAsPPM(scene.Name)
+}
+
+func GenerateRandomScene() world.Scene {
+	scene := world.NewScene("RandomScene")
+	scene.SetEnvironmentColour(render.Colour{Red: 135 / 256.0, Green: 206 / 256.0, Blue: 235 / 256.0})
+
+	// Floor
+	scene.AddEntity(entt.Sphere{
+		Position: maths.Point3{X: 0, Y: -1000, Z: -1},
+		Radius:   1000,
+		Material: mats.Lambertian{Albedo: render.Colour{Red: 0.5, Green: 0.5, Blue: 0.5}}})
+
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+
+	for i := -11; i < 11; i++ {
+		for j := -11; j < 11; j++ {
+			center := maths.Point3{
+				X: float64(i) + (0.9 * rng.Float64()),
+				Y: 0.2,
+				Z: float64(j) + (0.9 * rng.Float64())}
+
+			if center.Sub(maths.Point3{X: 4, Y: 0.2, Z: 0}).ToVec3().Length() > 0.9 {
+				chooseMat := rng.Float64()
+
+				if chooseMat < 0.8 {
+					scene.AddEntity(entt.Sphere{
+						Position: center,
+						Radius:   0.2,
+						Material: mats.Lambertian{Albedo: render.NewRandColour(rng)}})
+				} else if chooseMat < 0.95 {
+					scene.AddEntity(entt.Sphere{
+						Position: center,
+						Radius:   0.2,
+						Material: mats.Metal{
+							Albedo:    render.NewRandColour(rng),
+							Roughness: maths.Min(rng.Float64(), 0.7)}})
+				} else {
+					scene.AddEntity(entt.Sphere{
+						Position: center,
+						Radius:   0.2,
+						Material: mats.Dielectric{
+							Diffuse:         render.Colour{Red: 1.0, Green: 1.0, Blue: 1.0},
+							RefractionIndex: 1.5}})
+				}
+			}
+		}
+	}
+
+	scene.AddEntity(entt.Sphere{
+		Position: maths.Point3{X: 0, Y: 1, Z: 0},
+		Radius:   1.0,
+		Material: mats.Dielectric{
+			Diffuse:         render.Colour{Red: 1.0, Green: 1.0, Blue: 1.0},
+			RefractionIndex: 1.5}})
+
+	scene.AddEntity(entt.Sphere{
+		Position: maths.Point3{X: -4, Y: 1, Z: 0},
+		Radius:   1.0,
+		Material: mats.Lambertian{Albedo: render.Colour{Red: 0.4, Green: 0.2, Blue: 0.1}}})
+
+	scene.AddEntity(entt.Sphere{
+		Position: maths.Point3{X: 4, Y: 1, Z: 0},
+		Radius:   1.0,
+		Material: mats.Metal{
+			Albedo:    render.Colour{Red: 0.7, Green: 0.6, Blue: 0.5},
+			Roughness: 0}})
+
+	return scene
 }
